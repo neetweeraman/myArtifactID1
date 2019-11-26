1553851bfeb191f592ced46769189cb9b1822c68
neetweeraman <neet.weeraman@gmail.com>
2019-11-26 12:25:11 +0530

    src

---
+++ IdeaProjects/myArtifactID1/src/test/java/myGroupID1/tests/LoginTest.java
@@ -0,0 +1,31 @@
+package myGroupID1.tests;
+
+
+import myGroupID1.data.LoginData;
+import myGroupID1.functions.Login;
+import myGroupID1.utils.ExcelUtil;
+import myGroupID1.utils.TestBase;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class LoginTest extends TestBase {
+
+    @BeforeClass
+    public void init(ITestContext iTestContext) {
+        iTestContext.setAttribute("feature", "Login - ValidLogin");
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+
+        // Sample way to retrive data from excel
+        LoginData loginData = ExcelUtil.getLoginData("$as238l");
+
+        //UI Automation  sample
+        Login.loadLoginPage();
+        Login.quiteDriver();
+
+    }
+}
